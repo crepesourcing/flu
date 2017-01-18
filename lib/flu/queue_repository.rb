d705ca9bb2b0606a3a0dac9b965b85cbd08bf91b
Lorent Lempereur <lorent.lempereur.dev@gmail.com>
2017-01-18 11:29:12 +0100

    More capacities for read meta info

--- lib/flu/queue_repository.rb
+++ lib/flu/queue_repository.rb
@@ -10,8 +10,20 @@ module Flu
                                                       password: @configuration.rabbitmq_password)
     end
 
+    def find_all
+      @management_client.list_queues
+    end
+
     def find_queue(name)
       @management_client.queue_info("/", name)
+    end
+
+    def find_bindings_for_queue(name)
+      @management_client.list_queue_bindings("/", name)
+    end
+
+    def purge_queue(name)
+      @management_client.purge_queue("/", name)
     end
 
     def queue_names_that_match(matcher)
